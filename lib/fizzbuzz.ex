ddb36f38148d0fad52b59eab2644b95de9cd14c7
Benjamin Manford <benjaminmanford@gmail.com>
2020-03-03 05:45:55 +0800

    Remove comments

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -36,17 +36,12 @@ defmodule Fizzbuzz do
 
   """
   def multiples(n) do
-    # for number n in range 1 to number n, for every two seconds print return output
     Enum.each(1..n, fn n ->
       :timer.sleep(2000);
       cond do
-        # if number is both multiple of 3 and 5
         rem(n, 3) == 0 && rem(n, 5) == 0 -> IO.puts "Fizzbuzz"
-        # if number is multiple of 3
         rem(n, 3) == 0 -> IO.puts "Fizz"
-        # if number is multiple of 5
         rem(n, 5) == 0 -> IO.puts "buzz"
-        # if number is not either multiple of 3 or 5 then return the number
         true -> IO.puts n
       end
     end)
